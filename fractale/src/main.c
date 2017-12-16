064f3ea3e04b22528a294b26e6a9dd1d6844d8ad
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2017-12-16 14:47:12 +0100

    fractale help

--- fractale/src/main.c
+++ fractale/src/main.c
@@ -5,22 +5,20 @@
 ** Login   <alexandre@epitech.net>
 **
 ** Started on  Sat Dec 16 14:38:23 2017 alexandre Chamard-bois
-** Last update Sat Dec 16 14:38:25 2017 alexandre Chamard-bois
+** Last update Sat Dec 16 14:46:03 2017 alexandre Chamard-bois
 */
 
+#include <stdio.h>
 #include "my.h"
 
 int	main(int ac, char **av)
 {
 	t_patern patern;
 
-	if (ac > 3) {
-		my_puterr("Too many arguments.\n");
-		return (1);
-	}
-	if (ac < 3) {
-		my_puterr("Too few arguments.\n");
-		return (1);
+	if (ac != 3) {
+		printf("%s: size format\n", *av);
+		printf("\texemple: %s 3 \"###@# #@###\"\n", *av);
+		return (0);
 	}
 	if (parsing(av, &patern) || recup_patern(av, &patern)) {
 		return (1);
